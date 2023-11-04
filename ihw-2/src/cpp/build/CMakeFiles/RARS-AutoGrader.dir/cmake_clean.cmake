d41739f8ca299548ae6bd4eb8ad607c51ca2d193
Ambassador4ik <thebluedawn@outlook.com>
2023-11-04 19:42:00 +0300

    Add ihw-2

---
+++ ihw-2/src/cpp/build/CMakeFiles/RARS-AutoGrader.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RARS-AutoGrader.dir/Grader.cpp.o"
+  "CMakeFiles/RARS-AutoGrader.dir/Grader.cpp.o.d"
+  "RARS-AutoGrader"
+  "RARS-AutoGrader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RARS-AutoGrader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ihw-2/src/cpp/build/CMakeFiles/RARS-AutoGrader.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RARS-AutoGrader.
+# This may be replaced when dependencies are built.
